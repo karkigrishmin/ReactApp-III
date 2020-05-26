72d7b911a0eba30fef30d312bcceddf30f9065ce
Grishmin karki <grishmink@gmail.com>
2020-05-26 17:19:57 +0545

    Updates

---
+++ precache-manifest.450de2db877ec0278dc14a97853916be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff15304e14ed47ac1b2738ab96013a0",
+    "url": "/ReactApp-III/index.html"
+  },
+  {
+    "revision": "ca186758abd4c85dddd7",
+    "url": "/ReactApp-III/static/css/main.0ef3f016.chunk.css"
+  },
+  {
+    "revision": "71752e1f254d7fdddf9d",
+    "url": "/ReactApp-III/static/js/2.3f040f9e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactApp-III/static/js/2.3f040f9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca186758abd4c85dddd7",
+    "url": "/ReactApp-III/static/js/main.10d582b1.chunk.js"
+  },
+  {
+    "revision": "6a2dee6b06e14d3e7cd0",
+    "url": "/ReactApp-III/static/js/runtime-main.0d55c3c8.js"
+  }
+]);
